5ffe490db319df762e6bece2e8c0093cbf12fe6e
unknown <wallacefsp@outlook.pt>
2022-04-26 12:33:55 -0300

    a

---
+++ exe.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+<link rel="stylesheet" href="./css/style2.css">
+<center>
+<a <button type="button" href="index.php" class="button">VOLTAR</button></a>
+</center>
+</head>
+<body>
+    <center>
+<?php
+
+    if ($_POST['sort'] == '0' )
+    {
+        echo '<h1>Selecione Um Metódo De Ordenação</h1>' ;
+    }
+
+   $option = $_POST['sort'];
+
+   $vet = $vet[0]=$_POST['txt1']; $vet[1]=$_POST['txt2'];$vet[2]=$_POST['txt3']; $vet[3]=$_POST['txt4']; $vet[4]=$_POST['txt5'];
+
+   if ($option == '1')
+   {
+       $inicio = microtime(true);
+       function insertion_sort($vet){
+           $number_of_items = count($vet);
+           for($i = 0; $i <= $number_of_items - 2; $i++){
+               $k = $i;
+               for($j = $i + 1; $j > 0; $j--){
+                   if($vet[$j] < $vet[$k]){
+                       $temp = $vet[$j];
+                       $vet[$j] = $vet[$k];
+                       $vet[$k] = $temp;
+                   }
+                   if($k > 0 )
+                       $k--;
+               }
+           }
+           return $vet;
+       }
+
+       echo '<h1>Lista Organizada em Ordem Crescente Insert</h1>';
+       for($n=0;$n<5;$n++){
+           echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+       }
+
+       $fim = microtime(true);
+       $tempo = $fim - $inicio;
+       printf("Processado em: %0.16f segundos", $tempo/1000000);
+   }
+
+    if ($option == '2')
+    {
+        $inicio = microtime(true);
+        function mergesort($numlist)
+        {
+            if(count($numlist) == 1 ) return $numlist;
+
+            $mid = count($numlist) / 2;
+            $left = array_slice($numlist, 0, $mid);
+            $right = array_slice($numlist, $mid);
+
+            $left = mergesort($left);
+            $right = mergesort($right);
+
+            return merge($left, $right);
+        }
+
+        function merge($left, $right)
+        {
+            $leftIndex=0;
+            $rightIndex=0;
+
+            while($leftIndex<count($left) && $rightIndex<count($right))
+            {
+                if($left[$leftIndex]>$right[$rightIndex])
+                {
+
+                    $vet[]=$right[$rightIndex];
+                    $rightIndex++;
+                }
+                else
+                {
+                    $vet[]=$left[$leftIndex];
+                    $leftIndex++;
+                }
+            }
+            while($leftIndex<count($left))
+            {
+                $vet[]=$left[$leftIndex];
+                $leftIndex++;
+            }
+            while($rightIndex<count($right))
+            {
+                $vet[]=$right[$rightIndex];
+                $rightIndex++;
+            }
+            return $vet;
+        }
+
+        echo '<h1>Lista Organizada em Ordem Crescente Merge</h1>';
+        for($n=0;$n<5;$n++){
+            echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+
+        }
+
+        $fim = microtime(true);
+        $tempo = $fim - $inicio;
+        printf("Processado em: %0.16f segundos", $tempo/1000000);
+
+    }
+
+    if ($option == '3')
+    {
+        $inicio = microtime(true);
+        function quicksort($vet)
+        {
+            if (count($vet) < 2) {
+                return $vet;
+            }
+            $left = $right = array();
+            reset($vet);
+            $pivot_key = key($vet);
+            $pivot = array_shift($vet);
+            foreach ($vet as $k => $v) {
+                if ($v['times_ordered'] > $pivot['times_ordered']) {
+                    $left[$k] = $v;
+                } else {
+                    $right[$k] = $v;
+                }
+            }
+            return array_merge(quicksort($left), array($pivot_key => $pivot), quicksort($right));
+        }
+
+        echo '<h1>Lista Organizada em Ordem Crescente quick</h1>';
+        for($n=0;$n<5;$n++){
+            echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+
+        }
+
+        $fim = microtime(true);
+        $tempo = $fim - $inicio;
+        printf("Processado em: %0.16f segundos", $tempo/1000000);
+    }
+
+    if ($option == '4')
+    {
+        $inicio = microtime(true);
+        function selection_sort(&$arr) {
+            $n = count($arr);
+            for($i = 0; $i < count($arr); $i++) {
+                $min = $i;
+                for($j = $i + 1; $j < $n; $j++){
+                    if($arr[$j] < $arr[$min]){
+                        $min = $j;
+                    }
+                }
+                list($arr[$i],$arr[$min]) = array($arr[$min],$arr[$i]);
+            }
+        }
+
+        selection_sort($vet);
+
+        echo '<h1>Lista Organizada em Ordem Crescente Selection</h1>';
+        for($n=0;$n<5;$n++){
+            echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+
+        }
+
+        $fim = microtime(true);
+        $tempo = $fim - $inicio;
+        printf("Processado em: %0.16f segundos", $tempo/1000000);
+    }
+
+     if ($option == '5')
+     {
+         $inicio = microtime(true);
+         function shell_sort($arr) {
+             $inc = round(count($arr)/2);
+             while($inc > 0)      {
+                 for($i = $inc; $i < count($arr);$i++) {
+                     $temp = $arr[$i];
+                     $j = $i;
+                     while($j >= $inc && $arr[$j-$inc] > $temp) {
+                         $arr[$j] = $arr[$j - $inc];
+                         $j -= $inc;
+                     }
+                     $arr[$j] = $temp;
+                 }
+                 $inc = round($inc/2.2);
+             }
+             return $arr;
+         }
+
+         $sorted = shell_sort($vet);
+         print_r($sorted);
+
+         echo '<h1>Lista Organizada em Ordem Crescente Shell</h1>';
+         for($n=0;$n<5;$n++){
+             echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+
+         }
+
+         $fim = microtime(true);
+         $tempo = $fim - $inicio;
+         printf("Processado em: %0.16f segundos", $tempo/1000000);
+     }
+
+     if ($option == '6')
+     {
+         $inicio = microtime(true);
+         function bubble_sort($vet) {
+             $n = count($vet);
+             do {
+                 $swapped = false;
+                 for ($i = 0; $i < $n - 1; $i++) {
+                     // swap when out of order
+                     if ($vet[$i] > $vet[$i + 1]) {
+                         $temp = $vet[$i];
+                         $vet[$i] = $vet[$i + 1];
+                         $vet[$i + 1] = $temp;
+                         $swapped = true;
+                     }
+                 }
+                 $n--;
+             }
+             while ($swapped);
+             return $vet;
+         }
+
+         echo '<h1>Lista Organizada em Ordem Crescente Shell</h1>';
+         for($n=0;$n<5;$n++){
+             echo '<td><h3>Numero '.$n.'= '.$vet[$n].'</h3></td>';
+
+         }
+
+         $fim = microtime(true);
+         $tempo = $fim - $inicio;
+         printf("Processado em: %0.16f segundos", $tempo/1000000);
+
+     }
+
+?>
+</center>
+
+
+
+</body>
---
+++ index.php
@@ -0,0 +1,99 @@
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css"  href="estilo.css" />
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Trabalho ALGORITMOS</title>
+  </head>
+  <body>
+  	<div> </div>
+  	<center>
+   <form name="form1" method="post" action="exe.php">
+
+
+  <fieldset class="mb-3">
+  	
+    <legend><b>Tratativa de dados</b></legend>
+
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Posição 1 do vetor</label>
+      <input type="text" name="txt1" class="form-control" placeholder="Insira as informações a ser inserida ao vetor">
+    </div>
+
+
+      <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Posição 2 do vetor</label>
+      <input type="text" name="txt2" class="form-control" placeholder="Insira as informações a ser inserida ao vetor">
+    </div>
+
+      <select class="form-select" name="sort" aria-label="Default select example">
+          <option value="0">Selecione o Modo De Ordenação</option>
+          <option value="1">Insert Sort</option>
+          <option value="2">mergeSort</option>
+          <option value="3">quick Sort</option>
+          <option value="4">Selection Sort</option>
+          <option value="5">Shell Sort</option>
+          <option value="6">Buble Sort</option>
+      </select>
+
+   <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Posição 3 do vetor</label>
+      <input type="text" name="txt3" class="form-control" placeholder="Insira as informações a ser inserida ao vetor">
+    </div>
+
+
+   <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Posição 4 do vetor</label>
+      <input type="text" name="txt4" class="form-control" placeholder="Insira as informações a ser inserida ao vetor">
+    </div>
+
+   <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Posição 5 do vetor</label>
+      <input type="text" name="txt5" class="form-control" placeholder="Insira as informações a ser inserida ao vetor">
+    </div>
+
+
+    </div>
+    <div class="mb-3">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="2">
+        <label class="form-check-label" for="disabledFieldsetCheck">
+         <b> Li e concordo com nem sei o que...</b>
+        </label>
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary my-2">Submit</button>
+  </fieldset>
+  </div>
+</form>
+</center>
+
+</body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+
+  </body>
+  <div>
+ 
+ <center>
+ 	<b>
+</b>
+</center>
+
+    </div>
+</html>
